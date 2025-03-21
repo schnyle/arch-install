c537c17024e040c9a1f8a166e19a7c71c51328ce
kyle <kylesch115@gmail.com>
2025-03-20 21:53:14 -0600

    installPacmanPackage helper

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,41 @@
 
 namespace espaceCodes
 {
-  std::string reset = "\033[0m";
-  std::string green = "\033[32m";
-  std::string boldGreen = "\033[1;32m";
-  std::string red = "\033[31m";
-  std::string boldRed = "\033[1;31m";
-}
+std::string reset = "\033[0m";
+std::string green = "\033[32m";
+std::string boldGreen = "\033[1;32m";
+std::string red = "\033[31m";
+std::string boldRed = "\033[1;31m";
+} // namespace espaceCodes
 
-std::vector<std::string>
-    pacmanPackages({
-        "alacritty",
-        "arandr",
-        "cmake",
-        "git",
-        "base-devel",
-        "i3",
-        "lib32-nvidia-utils",
-        "nano",
-        "networkmanager",
-        "nvidia",
-        "nvidia-utils",
-        "nvidia-settings",
-        "openssh",
-        "pavucontrol",
-        "picom",
-        "pulseaudio",
-        "steam",
-        "tmux",
-        "vim",
-        "xclip",
-        "xorg-server",
-        "xorg-init",
-        "xorg-apps",
-    });
+std::vector<std::string> pacmanPackages({
+    "vim",
+    "fjaskd",
+    "nano",
+    // "alacritty",
+    // "arandr",
+    // "cmake",
+    // "git",
+    // "base-devel",
+    // "i3",
+    // "lib32-nvidia-utils",
+    // "nano",
+    // "networkmanager",
+    // "nvidia",
+    // "nvidia-utils",
+    // "nvidia-settings",
+    // "openssh",
+    // "pavucontrol",
+    // "picom",
+    // "pulseaudio",
+    // "steam",
+    // "tmux",
+    // "vim",
+    // "xclip",
+    // "xorg-server",
+    // "xorg-init",
+    // "xorg-apps",
+});
 
 struct CommandResult
 {
@@ -71,18 +73,31 @@ CommandResult executeCommand(const std::string &command)
   return result;
 }
 
+void installPacmanPackage(const std::string &package)
+{
+  static const std::string successText = espaceCodes::boldGreen + "success" + espaceCodes::reset;
+  static const std::string failureText = espaceCodes::boldRed + "failure" + espaceCodes::reset;
+
+  std::string command = "sudo pacman -S --noconfirm " + package;
+  std::cout << "Installing " << package << "... ";
+
+  CommandResult result = executeCommand(command);
+
+  std::string status = result.success ? successText : failureText;
+  std::cout << "[" << status << "]\n";
+
+  if (!result.success)
+  {
+    std::cout << "Error installing package: " << package << ":\n" << result.output;
+  }
+}
+
 int main()
 {
   std::cout << "~~~Installing Arch Linux~~~\n";
-  std::cout << "Installing nano... " << std::flush;
-  std::string command = "sudo pacman -S --noconfirm nano 2>&1";
 
-  auto result = executeCommand(command);
-
-  std::string status = result.success ? espaceCodes::boldGreen + "success" : espaceCodes::boldRed + "failure";
-  std::cout << "[" << status << espaceCodes::reset << "]\n";
-  if (!result.success)
+  for (const auto &packageName : pacmanPackages)
   {
-    std::cout << result.output << "\n";
+    installPacmanPackage(packageName);
   }
 }
